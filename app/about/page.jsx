d7bed5c74daa418019415b1df936f2ed2ac0f90f
alen225673 <alen225673@sahrdaya.ac.in>
2026-07-24 03:54:31 +0000

    added about page

---
+++ app/about/page.jsx
@@ -0,0 +1,9 @@
+export default function AboutPage(){
+    return(
+        <>
+        <h1>This is my first next js assignment</h1>
+         <h1>Alen Davis CSEA BTECH</h1>
+        
+    </>
+    );
+}
